647bf3901c82955b071c9e69805b0832f822d64d
HXXNS <39939869+HXXNS@users.noreply.github.com>
2020-02-16 23:41:30 +0900

    Prisma Env

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -1,5 +1,4 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { USER_FRAGMENT } from "../../../fragments";
 
 export default {
   Query: {
@@ -7,14 +6,14 @@ export default {
       console.log("user parent", _);
       isAuthenticated(request);
       const { user } = request;
-      const userProfile = prisma.user({ id: user.id });
+      return await prisma.user({ id: user.id });
       //fragments 사용  deep relation 방지
       //return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
-      const posts = await prisma.user({ id: user.id }).posts();
-      return {
-        user: userProfile,
-        posts
-      };
+      // const posts = await prisma.user({ id: user.id }).posts();
+      // return {
+      //   user: userProfile,
+      //   posts
+      // };
     }
   }
 };
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -11,6 +11,7 @@ export default {
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
       } catch (error) {
+        console.log(error);
         return false;
       }
     }
